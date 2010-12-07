51d876cbbbd6c2ecdca9cc8f9fe9f296b280ae0f
Andrew Watts <awatts@bcs.rochester.edu>
2010-12-07 12:48:37 -0500

    Upped the number of generated form fields to match the size of Florian's lists. Added allowScriptAccess=always to the SWFObject embed call so that it will work on mturk

--- flash_expt.js
+++ flash_expt.js
@@ -13,7 +13,7 @@ jQuery(document).ready(function(){
                 }
             );
     var resultform = jQuery('#results');
-    var trialcount = 32; //this is kind of a stab in the dark. I don't know how many trials to expect.
+    var trialcount = 68; //this is kind of a stab in the dark. I don't know how many trials to expect.
     for(var i=1;i<=trialcount;i++) {
         resultform.append('<input type="hidden" name="trial' + i + '" id="trial' + i + '" value="NA" /> ');
     }
