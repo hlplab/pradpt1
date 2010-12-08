aee2508a337c89d4b04069eeb3380876c5c10578
Andrew Watts <awatts@bcs.rochester.edu>
2010-12-08 11:08:03 -0500

    Some changes to the templates and JS

--- flash_expt.js
+++ flash_expt.js
@@ -4,14 +4,11 @@ function register(field, answer) {
 }
 
 jQuery(document).ready(function(){
-    jQuery('#content').hide();
-    jQuery('input#consentcx').click(function(){
-                if (jQuery(this).attr('checked') === true) {
-                    jQuery('#instructions').hide();
-                    jQuery('#content').show();
-                    }
-                }
-            );
+    jQuery('input#consentcx').change(function(){
+        jQuery('#instructions').hide();
+        jQuery('#content').show();
+    });
+
     var resultform = jQuery('#results');
     var trialcount = 68; //this is kind of a stab in the dark. I don't know how many trials to expect.
     for(var i=1;i<=trialcount;i++) {
